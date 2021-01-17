21e6c7ac0c25a0a05dff8f9835521332551d5049
Udyan Sharma <udyan2@gmail.com>
2021-01-18 02:41:40 +0530

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -1,11 +1,13 @@
 import datetime as dt
 
-if dt.datetime.today().hour>=6 and dt.datetime.today().hour<12:
-    print("Good Morning")
-if dt.datetime.today().hour>=12 and dt.datetime.today().hour<18:
-    print("Good Afternoon")
-if dt.datetime.today().hour>=18 and dt.datetime.today().hour<22:
-    print("Good Evening")
-if (dt.datetime.today().hour>=22 and dt.datetime.today().hour<=24) or dt.datetime.today().hour==0:
-    print("It's midnight")
-if(dt.datetime.today())==today.time()
+def greet(): 
+    if dt.datetime.today().hour>=0 and dt.datetime.today().hour<6:
+        return "It is late at night."
+    if dt.datetime.today().hour>=6 and dt.datetime.today().hour<12:
+        return "Good morning!"
+    elif dt.datetime.today().hour>=12 and dt.datetime.today().hour<18:
+        return "Good afternoon!"
+    elif dt.datetime.today().hour>=18 and dt.datetime.today().hour<22:
+        return "Good evening!"
+    elif (dt.datetime.today().hour>=22 and dt.datetime.today().hour<=24) or dt.datetime.today().hour==0:
+        return "It's night"
