1e3efd703aa98856e63b94c5f584995eea89f058
SuryaRaghuvanshi02 <73468842+SuryaRaghuvanshi02@users.noreply.github.com>
2021-01-26 00:06:46 +0530

    Final xc and tikinter removed

--- xc.py
+++ xc.py
@@ -13,121 +13,116 @@ import pyjokes
 import pyautogui
 import pywhatkit
 import pyttsx3
+import record
 eng = pyttsx3.init()
-def speak(text):
-    tts = gTTS(text = text, lang="en-UK")
-    filename = "voice1.mp3"
-    tts.save(filename)
-    playsound.playsound(filename)
-    os.remove(filename)
-def gudio():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        audio = r.listen(source)
-        said = ""
+voice_in=record.recorder()
 
-        try:
-            said = r.recognize_google(audio)
-            print(said)
-        except Exception as e:
-            print("Exception: "+str(e))
-    return said.lower()
-text = gudio()
-if 'hello' in text:
-    speak("Hi, How are you?")
-if 'what is the time' in text:
-    def time_():
-        Time = datetime.datetime.now().strftime("%I:%M:%S")
-        speak("The current time is")
-        speak(Time)
-    time_()
-if 'search on wikipedia about' in text:
-    speak("Searching.....")
-    text = text.replace('search on wikipedia about','')
-    result = wikipedia.summary(text,sentences = 2)
-    speak('According to Wikipedia')
+def time_():
+    Time = datetime.datetime.now().strftime("%I:%M:%S")
+    eng.say("The current time is")
+    eng.runAndWait()
+    eng.say(Time)
+    eng.runAndWait()
+def wikipedia_():
+    eng.say("Searching.....")
+    eng.runAndWait()
+    voice_in = voice_in.replace('search on wikipedia about','')
+    result = wikipedia.summary(voice_in,sentences = 2)
+    eng.say('According to Wikipedia')
+    eng.runAndWait()
     print(result)
-    speak(result)
-if 'send an email' in text:
-    def sendinganemail(to,content):
-        server = smtplib.SMTP('smtp.gmail.com',587)
-        server.ehlo()
-        server.starttls()
-        ## need low security for email.
+    eng.say(result)
+    eng.runAndWait()
+def sendinganemail(to,content):
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.ehlo()
+    server.starttls()
+    ## need low security for email.
 
-        server.login('suryawolfgaming@gmail.com','passsword')
-        server.close()
-    try:
-        speak("Whome should i send it to?")
-        reciever = input('Email of reciever:')
-        speak("what should i say?")
-        content = gudio()
-        to = reciever
-        sendinganemail(to,content)
-        speak(content)
-        speak('Email has been sent')
-    except Exception as e:
-        print(e)
-        speak('Unable to send email')  
-if "cpu" in text:
-    def cpuusage():
-        usage = str(psutil.cpu_percent())
-        speak('CPU is at'+usage)
-    cpuusage()
-if "battery" in text:
+    server.login('suryawolfgaming@gmail.com','passsword')
+    server.close()
+try:
+    eng.say("Whome should i send it to?")
+    eng.runAndWait()
+    reciever = input('Email of reciever:')
+    eng.say("what should i say?")
+    eng.runAndWait()
+    content = voice_in()
+    to = reciever
+    sendinganemail(to,content)
+    eng.say(content)
+    eng.runAndWait()
+    eng.say('Email has been sent')
+    eng.runAndWait()
+except Exception as e:
+    print(e)
+    eng.say('Unable to send email')  
+    eng.runAndWait()
+
+def cpuusage():
+    usage = str(psutil.cpu_percent())
+    eng.say('CPU is at'+usage)
+    eng.runAndWait()
+def battery_():
     battery = psutil.sensors_battery()
     eng.say('Battery is at')
     eng.runAndWait()
     eng.say(battery.percent)
     eng.runAndWait()
-if "tell me a joke" in text:
-    def joke():
-        joke = pyjokes.get_joke()
-        speak(joke)
-        print(joke)
-    joke()
-if "bye bye" in text:
-    speak("Going Offline")
+def joke():
+    joke = pyjokes.get_joke()
+    eng.say(joke)
+    eng.runAndWait
+    print(joke)
+def byebye():
+    eng.say("Going Offline")
+    eng.runAndWait()
     quit()
-if 'word' in text:
-    speak("Opening Word....")
+def word_():
+    eng.say("Opening Word....")
+    eng.runAndWait()
     msworld = r"C:/Program Files/Microsoft Office/root/Office16/WINWORD.EXE"
     os.startfile(msworld)
-if 'excel' in text:
-    speak("Opening Excel.....")
+def excel_():
+    eng.say("Opening Excel.....")
+    eng.runAndWait()
     msexcel = r"C:/Program Files/Microsoft Office/root/Office16/EXCEL.EXE"
     os.startfile(msexcel)## what should i search
-if 'write a note' in text:
-    def note(text):
-        file_name = "samplenote.txt"
-        with open(file_name,"w") as f:
-            f.write(text)
-        subprocess.Popen(["notepad.exe", file_name])
+def note(voice_in):
+    file_name = "samplenote.txt"
+    with open(file_name,"w") as f:
+        f.write(voice_in)
+    subprocess.Popen(["notepad.exe", file_name])
 
-    speak("what should I write")
-    note_text = gudio()
-    note(note_text)
-    speak("done")
-if 'vs code' in text:
-    speak("Opening VSCode....")
+eng.say("what should I write")
+eng.runAndWait()
+note_voice_in = voice_in()
+note(note_voice_in)
+eng.say("done")
+def vscode():
+    eng.say("Opening VSCode....")
+    eng.runAndWait()
     Vscode = r"C:/Users/LENOVO/AppData/Local/Programs/Microsoft VS Code/Code.exe"
     os.startfile(Vscode)
-if 'screenshot' in text:
-    def screenshot():
-        img = pyautogui.screenshot()
-        img.save('F:/screenshot.png')
-        speak("Done.........")
-    screenshot()
+def screenshot():
+    img = pyautogui.screenshot()
+    img.save('F:/screenshot.png')
+    eng.say("Done.........")
+    eng.runAndWait()
 ########### Whatsapp message at a particular time##################
-if "play" in text:
-    speak("To which number")
-    whatnum = gudio()
-    speak("what is the message?")
-    whatmessage = gudio()
-    speak("At what Hour?")
-    whatHour = gudio()
-    speak("At what Min?")
-    whatmin = gudio()
+if "play" in voice_in:
+    eng.say("To which number")
+    eng.runAndWait()
+    whatnum = voice_in()
+    eng.say("what is the message?")
+    eng.runAndWait()
+    whatmessage = voice_in()
+    eng.say("At what Hour?")
+    eng.runAndWait()
+    whatHour = voice_in()
+    eng.say("At what Min?")
+    eng.runAndWait()
+    whatmin = voice_in()
     whatHour1 = eval(whatHour)
     whatmin1 = eval(whatmin)
     pywhatkit.sendwhatmsg('+91{}',format(whatnum),whatmessage,whatHour1,whatmin1)
